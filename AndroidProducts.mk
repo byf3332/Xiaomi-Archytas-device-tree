24a2c3793c296959ed45b7e703e4c51a355e92be
byf3332 <71202267+byf3332@users.noreply.github.com>
2024-02-01 12:10:03 +0800

    initial upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Archytas.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Archytas-user \
+    omni_Archytas-userdebug \
+    omni_Archytas-eng
---
+++ omni_Archytas.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Archytas device
+$(call inherit-product, device/xiaomi/Archytas/device.mk)
+
+PRODUCT_DEVICE := Archytas
+PRODUCT_NAME := omni_Archytas
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Archytas
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-longcheer
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_Archytas-user 9 PPR1.180610.011 1754 release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/full_Archytas/Archytas:9/PPR1.180610.011/1754:user/release-keys
